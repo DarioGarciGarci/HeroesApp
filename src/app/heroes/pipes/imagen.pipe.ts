8befe10a6dafd7d9c8308095519b4ee654584489
Darío <dariogarcigarci@gmail.com>
2022-06-15 13:03:55 +0200

    Modificacion comentarios pipe

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -8,6 +8,7 @@ export class ImagenPipe implements PipeTransform {
 
 //https://i.pinimg.com/736x/fa/c0/43/fac04382be00644bcdb7282ea127de09.jpg
 //https://cloudfront-us-east-1.images.arcpublishing.com/infobae/NLXD7LVC7BDA3OFMVY4Z6HYJBU.jpg
+//https://i.pinimg.com/originals/10/3f/11/103f119f037c88f308cbed5e452e5fdf.jpg
 
   transform(heroe: Heroe): string {
     if(!heroe.id && !heroe.alt_img){
